502a72c8c98f0f74618c3b220eb1c73435e44183
Bsh <dnfg12@naver.com>
2024-07-08 15:38:44 +0900

    second commit to deploy

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+
+interface IParams {
+  params: { id: string };
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetails({
   params: { id },
-}: {
-  params: { id: string };
-}) {
+}: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,17 +2,19 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <Link href="/">Home</Link> {path === "/" ?  "❤": ""}
+          <Link href="/">Home</Link> {path === "/" ? "❤" : ""}
         </li>
         <li>
-          <Link href="/about-us">About Us</Link> {path === "/about-us" ?  "❤": ""}
+          <Link href="/about-us">About Us</Link>{" "}
+          {path === "/about-us" ? "❤" : ""}
         </li>
       </ul>
     </nav>
